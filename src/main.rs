532d50eb4f23d7cef7b5f0dc13db7f4b19cd5a90
Robin Herrmann <Robin.Herrmann@dormakaba-external.com>
2023-05-17 17:20:09 +0200

    adaptions for article

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-pub use lambda_http::aws_lambda_events::{serde::Deserialize, serde_json};
+pub use lambda_http::aws_lambda_events::{serde::Deserialize, serde::Serialize, serde_json};
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde_json::Value;
 
@@ -8,17 +8,34 @@ async fn main() -> Result<(), Error> {
     lambda_runtime::run(func).await?;
     Ok(())
 }
+#[derive(Serialize, Deserialize)]
+struct Payload {
+    message: String,
+}
 
-async fn func(_: LambdaEvent<Value>) -> Result<(), Error> {
+async fn func(value: LambdaEvent<Value>) -> Result<(), Error> {
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_iotdataplane::Client::new(&config);
-    let message = aws_sdk_iotdataplane::types::Blob::new("input");
-    client
-        .publish()
-        .topic("output")
-        .qos(1)
-        .payload(message)
-        .send()
-        .await?;
+    let result: Result<Payload, _> = serde_json::from_str(value.payload.to_string().as_str());
+    match result {
+        Ok(payload) => {
+            if payload.message == "Hello from AWS IoT console" {
+                let greeting = Payload {
+                    message: "Hello from AWS Lambda".into(),
+                };
+                let answer = aws_sdk_iotdataplane::types::Blob::new(
+                    serde_json::to_string(&greeting).unwrap(),
+                );
+                client
+                    .publish()
+                    .topic("example/topic")
+                    .qos(1)
+                    .payload(answer)
+                    .send()
+                    .await?;
+            }
+        }
+        Err(_) => {}
+    }
     Ok(())
 }
